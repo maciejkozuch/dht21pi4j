ce84daf5b4c0eec7de9c6d781f8648ca47ddec87
Maciej Kożuch <maciej.kozuch@gmail.com>
2022-04-08 00:22:19 +0200

    The project was configured like from pi4j-maven-archetype (https://github.com/Pi4J/pi4j-maven-archetype).

--- src/main/java/com/maciejkozuch/dht21pi4j/DHT21Pi4j.java
+++ src/main/java/com/maciejkozuch/dht21pi4j/DHT21Pi4j.java
@@ -2,30 +2,49 @@ package com.maciejkozuch.dht21pi4j;
 
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 import static com.maciejkozuch.dht21pi4j.SensorState.*;
 
-public class DHT21Pi4j {
+public class DHT21Pi4j implements AutoCloseable {
 
     private SensorState state;
     private Instant sensorStarted;
 
     private Context context;
 
+    final Logger logger = LoggerFactory.getLogger(DHT21Pi4j.class);
+
     public DHT21Pi4j() {
+        logger.trace("The DHT21 sensor is being initialized.");
         sensorStarted = Instant.now();
         state = WAITING_TO_START;
 
         context = Pi4J.newAutoContext();
+        logger.debug("The DHT21 sensor was initialized.");
     }
 
     public TemperatureAndHumidityValue readValue() {
         waitIfNotInitialized();
 
+        logger.debug("The temperature and humidity was red from the sensor");
         return new TemperatureAndHumidityValue();
+    }
+
+    @Override
+    public void close() {
+        context.shutdown();
+
+    }
+
+    public static void main(String[] args) {
+        try (DHT21Pi4j dht21Pi4j = new DHT21Pi4j()) {
+            dht21Pi4j.readValue();
+        }
     }
 
     private void waitIfNotInitialized() {
@@ -33,11 +52,11 @@ public class DHT21Pi4j {
             if(Instant.now().getEpochSecond() - sensorStarted.getEpochSecond() >= 1) {
                 state = READY;
             } else {
+                logger.debug("The system is waiting 1s to prepare the DHT21 sensor");
                 try {
                     TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {}
             }
         }
     }
-
 }
